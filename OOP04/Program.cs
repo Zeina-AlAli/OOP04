e624dac8c70f4b8f7fdb5dc45e2e1d81b6e4d41b
Zeina-AlAli <zeinamahmoud995@gmail.com>
2026-08-18 08:28:40 +0300

    Question01

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 :
+            //// a.What is Abstraction?
+            //Console.WriteLine("Abstraction: is the process of hiding the implementation details and showing only the essential features to the user ");
+
+            //// b.Why abstraction is considered one of the 4 pillars
+            //Console.WriteLine("because it reduce complexity, improves maintainability, enhance security, and promote flexibility ");
+
+            #endregion
+
         }
     }
 }
