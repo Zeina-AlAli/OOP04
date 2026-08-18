959187128516a7b683c6634e88fd0ca4cb0167f6
Zeina-AlAli <zeinamahmoud995@gmail.com>
2026-08-18 08:46:16 +0300

    Question02

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -13,6 +13,16 @@
 
             #endregion
 
-        }
+            #region Q2 :
+            //// a. Abstract vs interface class
+            //Console.WriteLine("Abstract class can have: absract (unimplemented) and concrete methods, fields, constructor,and can use any access modifier. in addition, class can inherit one abstract class \nwhile interface have only abstract methods, cant have fields or constructor, members are public by default. classes can implement multiple interface.");
+
+            //// b. Choose interface instead od abstract class
+            //Console.WriteLine("When you want to define a concract that unrelated classes can implement, without sharing common implentation ");
+
+            //// c. multiple inheritance
+            //Console.WriteLine("Classes cant inherit multiple abstract classes, but can implement multiple interface");
+            #endregion
+        } 
     }
 }
